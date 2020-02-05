5756d9072d443f966359a97a07d330cc812a0329
Bren Briggs <code@fraq.io>
2020-02-05 15:31:57 -0500

    Add cloudfront distribution, ACM, and redirect route53
    This is a very large patch that really couldn't be broken down any
    smaller than this.
    
    What this does:
    1. Creates an ACM resource that generates certificates automatically on
       behalf of the domain requested
    2. Creates a cloudfront distribution that uses the S3 static site as the
       source
    3. Uses the ACM resource's generated certificate for TLS
    4. Points the route53 record for the website at _cloudfront_ instead of
       at the bucket. This is another major change.
    5. Exposes new variables for cloudfront that can be tweaked, but no new
       required variables are introduced.

---
+++ cloudfront.tf
@@ -0,0 +1,50 @@
+resource "aws_cloudfront_origin_access_identity" "this" {
+  comment = "Origin Access Identity for ${var.app_hostname}"
+}
+
+resource "aws_cloudfront_distribution" "this" {
+  origin {
+    domain_name = aws_s3_bucket.this.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.this.bucket
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.this.cloudfront_access_identity_path
+    }
+  }
+
+  aliases = [var.app_hostname]
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Cloudfront Distribution for ${var.app_hostname}"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = var.allowed_methods
+    cached_methods   = var.cached_methods
+    target_origin_id = aws_s3_bucket.this.bucket
+
+    forwarded_values {
+      query_string = var.forward_query_string
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = var.min_ttl
+    default_ttl            = var.default_ttl
+    max_ttl                = var.max_ttl
+  }
+
+  price_class = var.cloudfront_price_class
+
+  viewer_certificate {
+    acm_certificate_arn = module.acm.this_certificate_arn
+  }
+
+  tags {
+    Name = var.app_hostname
+  }
+}
