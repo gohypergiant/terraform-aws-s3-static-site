dae8608a4730f582df7aa9f0201eb926862a68f1
Bren Briggs <code@fraq.io>
2020-05-06 22:11:25 -0400

    Add encryption and versioning to log bucket

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "random_id" "iam" {
 }
 
 resource "aws_s3_bucket" "this" {
-  // checkov:skip=CKV_AWS_19: Do not encrypt public buckets
   // checkov:skip=CKV_AWS_20: Public bucket should have public-read
   // checkov:skip=CKV_AWS_52: AWS+TF do not properly support mfa_delete
   bucket = var.bucket_name
@@ -23,11 +22,33 @@ resource "aws_s3_bucket" "this" {
     target_prefix = "${var.log_prefix}/s3"
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket" "logs" {
+  // checkov:skip=CKV_AWS_18:Log buckets don't need to export their logs
+  // checkov:skip=CKV_AWS_52: AWS+TF do not properly support mfa_delete
   bucket = var.log_bucket_name
   acl    = "log-delivery-write"
+
+  versioning {
+    enabled = var.bucket_versioning
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
 }
 
 resource "aws_route53_record" "this" {
